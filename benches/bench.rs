9390c6e0b977466a104c82a5e9b26117fe9e380f
Arturo Castro <arturo@rin.rs>
2023-10-23 14:59:01 +0200

    interleave three benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,15 +4,42 @@
 mod benches {
     use std::mem::size_of;
 
+    use wasm_parser::thin_slice::ThinSlice;
+
     extern crate test;
     extern crate wasm_parser;
 
     #[bench]
     fn copy(b: &mut test::Bencher) {
-        let src = vec![0f32;1_000_000];
-        let mut dst = vec![0f32;1_000_000];
+        let src0 = vec![0f32; 333_333];
+        let src1 = vec![0f32; 333_333];
+        let src2 = vec![0f32; 333_333];
+        let mut dst = vec![0f32; 999_999];
         b.iter(|| {
-            wasm_parser::interleaved::copy_to_interleaved_array(&mut dst, &src, 0, size_of::<f32>(), 0, src.len());
+            wasm_parser::interleaved::copy_to_interleaved_array(&mut dst, &src0, size_of::<f32>() * 0, size_of::<f32>() * 3, 0, src0.len());
+            wasm_parser::interleaved::copy_to_interleaved_array(&mut dst, &src1, size_of::<f32>() * 1, size_of::<f32>() * 3, 0, src1.len());
+            wasm_parser::interleaved::copy_to_interleaved_array(&mut dst, &src2, size_of::<f32>() * 2, size_of::<f32>() * 3, 0, src2.len());
+        });
+    }
+
+    #[bench]
+    fn copy_three(b: &mut test::Bencher) {
+        let data0 = vec![0f32; 333_333];
+        let data1 = vec![0f32; 333_333];
+        let data2 = vec![0f32; 333_333];
+        let src0 = ThinSlice::from_data_and_offset(bytemuck::cast_slice(&data0), 0, (data0.len() * size_of::<f32>()) as u32);
+        let src1 = ThinSlice::from_data_and_offset(bytemuck::cast_slice(&data1), 0, (data1.len() * size_of::<f32>()) as u32);
+        let src2 = ThinSlice::from_data_and_offset(bytemuck::cast_slice(&data2), 0, (data2.len() * size_of::<f32>()) as u32);
+        let mut dst = vec![0f32; 999_999];
+        b.iter(|| {
+            wasm_parser::interleaved::interleave_three(
+                &mut dst,
+                unsafe{ src0.as_slice(data0.len() as u32) },
+                unsafe{ src1.as_slice(data1.len() as u32) },
+                unsafe{ src2.as_slice(data2.len() as u32) },
+                size_of::<f32>() * 0,
+                size_of::<f32>() * 3,
+            );
         });
     }
 
