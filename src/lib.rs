ae784ab1672d7eeb9ec875b633df794c35508ab3
Arturo Castro <arturo@rin.rs>
2023-10-31 15:25:56 +0100

    add wasm bingen intersection benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 // #![cfg_attr(target_family="wasm", no_std)]
+#![cfg_attr(feature="wasm-bindgen-bench", feature(test))]
 
 #[cfg(feature="cap")]
 use std::alloc;
@@ -309,4 +310,88 @@ impl Drop for Schema {
 
         unsafe{ &mut *self.arena.0 }.reset();
     }
+}
+
+#[cfg(feature="wasm-bindgen-bench")]
+#[wasm_bindgen]
+pub fn bench_intersections() {
+    extern crate test;
+    use glam::*;
+    use rand::random;
+    use std::cell::UnsafeCell;
+
+    const INTERSECTIONS_LEN: usize = 1_000_000;
+
+    fn model_local_matrix(local_space_translation: Vec3, offset: Vec3, scale: f32) -> Mat4 {
+        let (ox, oy, oz) = (offset.x, offset.y, offset.z);
+        let (tx, ty, tz) = (local_space_translation.x, local_space_translation.y, local_space_translation.z);
+        mat4(
+            vec4(scale, 0., 0., 0.),
+            vec4(0., scale, 0., 0.),
+            vec4(0., 0., scale, 0.),
+            vec4(ox - tx, oy - ty, oz - tz, 1.),
+        )
+    }
+
+    let pos = (0..INTERSECTIONS_LEN).flat_map(|_| {
+        let p0 = (random::<i16>(), random::<i16>(), random::<i16>());
+        let p1 = (random::<i16>(), random::<i16>(), random());
+        let p2 = (random::<i16>(), random::<i16>(), random::<i16>());
+        [p0.0, p0.1, p0.2, p1.0, p1.1, p1.2, p2.0, p2.1, p2.2]
+    }).collect::<Vec<_>>();
+
+    let sequential_idx = (0 .. INTERSECTIONS_LEN as u32).flat_map(|i|
+        [i * 3 + 0, i * 3 + 1, i * 3 + 2]
+    ).collect::<Vec<_>>();
+
+    let random_offset_idx = (0 .. INTERSECTIONS_LEN as u32).flat_map(|i| {
+        let idx_offset = ((random::<f32>() * 2. - 1.) * 100.) as u32;
+        let i = (i + idx_offset).min(INTERSECTIONS_LEN as u32 - 1);
+        [i * 3 + 0, i * 3 + 1, i * 3 + 2]
+    }).collect::<Vec<_>>();
+
+    let random_idx = (0 .. INTERSECTIONS_LEN).flat_map(|_| {
+        let i = (random::<f32>() * INTERSECTIONS_LEN as f32) as u32;
+        [i * 3 + 0, i * 3 + 1, i * 3 + 2]
+    }).collect::<Vec<_>>();
+
+    let output = vec![0f32; INTERSECTIONS_LEN * 10];
+    let output = UnsafeCell::new(output);
+
+    let local_space_translation = vec3(100., 10., 1000.);
+    let offset = vec3(10., 1000., 10000.);
+    let scale = 3.;
+    let plane_normal = vec3(1., 3., 5.).normalize();
+    let plane_offset = 2000.;
+    let plane = vec4(plane_normal.x, plane_normal.y, plane_normal.z, plane_offset);
+    let model_local_matrix = model_local_matrix(local_space_translation, offset, scale);
+    let (plane_local_matrix, local_plane_matrix) = crate::outlines::plane_matrices(plane, local_space_translation);
+    let model_to_plane_mat = local_plane_matrix * model_local_matrix * crate::outlines::DENORM_MATRIX;
+
+    crate::log!("sequential idx: {}", easybench_wasm::bench(|| {
+        let idx = test::black_box(sequential_idx.as_slice());
+        let pos = test::black_box(pos.as_slice());
+        let model_to_plane_mat = test::black_box(model_to_plane_mat);
+        let output = test::black_box(unsafe{ &mut *output.get() });
+        crate::outlines::intersect_triangles(idx, pos, model_to_plane_mat, output);
+        output
+    }));
+
+    crate::log!("random offset idx: {}", easybench_wasm::bench(|| {
+        let idx = test::black_box(random_offset_idx.as_slice());
+        let pos = test::black_box(pos.as_slice());
+        let model_to_plane_mat = test::black_box(model_to_plane_mat);
+        let output = test::black_box(unsafe{ &mut *output.get() });
+        crate::outlines::intersect_triangles(idx, pos, model_to_plane_mat, output);
+        output
+    }));
+
+    crate::log!("random idx: {}", easybench_wasm::bench(|| {
+        let idx = test::black_box(sequential_idx.as_slice());
+        let pos = test::black_box(pos.as_slice());
+        let model_to_plane_mat = test::black_box(model_to_plane_mat);
+        let output = test::black_box(unsafe{ &mut *output.get() });
+        crate::outlines::intersect_triangles(idx, pos, model_to_plane_mat, output);
+        output
+    }));
 }
