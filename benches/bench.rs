f96d2638fdaf8bd8bbcba30dea1ff2ce73a2cf70
Arturo Castro <arturo@rin.rs>
2023-10-31 10:30:15 +0100

    benchmark triangle intersections

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,7 +4,9 @@
 mod benches {
     use std::mem::size_of;
 
-    use wasm_parser::thin_slice::ThinSlice;
+    use glam::Vec3;
+    use rand::random;
+    use wasm_parser::{thin_slice::ThinSlice, Arena};
 
     extern crate test;
     extern crate wasm_parser;
@@ -108,6 +110,56 @@ mod benches {
                 &wasm_parser::parser::Highlights::new(arena),
                 |_| true
             );
+        });
+    }
+
+    #[bench]
+    fn intersections(b: &mut test::Bencher) {
+        use glam::*;
+
+        fn model_local_matrix(local_space_translation: Vec3, offset: Vec3, scale: f32) -> Mat4 {
+            let (ox, oy, oz) = (offset.x, offset.y, offset.z);
+            let (tx, ty, tz) = (local_space_translation.x, local_space_translation.y, local_space_translation.z);
+            mat4(
+                vec4(scale, 0., 0., 0.),
+                vec4(0., scale, 0., 0.),
+                vec4(0., 0., scale, 0.),
+                vec4(ox - tx, oy - ty, oz - tz, 1.),
+            )
+        }
+
+        let pos = (0..1_000_000).flat_map(|_| {
+            let p0 = (random::<i16>(), random::<i16>(), random::<i16>());
+            let p1 = (random::<i16>(), random::<i16>(), random());
+            let p2 = (random::<i16>(), random::<i16>(), random::<i16>());
+            [p0.0, p0.1, p0.2, p1.0, p1.1, p1.2, p2.0, p2.1, p2.2]
+        }).collect::<Vec<_>>();
+
+        let idx = (0u32 .. 1_000_000).flat_map(|i|
+            [i * 3 + 0, i * 3 + 1, i * 3 + 2]
+        ).collect::<Vec<_>>();
+
+        let mut arena = Arena::new();
+
+        let mut output = vec![0.; 10_000_000];
+
+        let local_space_translation = vec3(100., 10., 1000.);
+        let offset = vec3(10., 1000., 10000.);
+        let scale = 3.;
+        let plane_normal = vec3(1., 3., 5.).normalize();
+        let plane_offset = 2000.;
+        let plane = vec4(plane_normal.x, plane_normal.y, plane_normal.z, plane_offset);
+        let model_local_matrix = model_local_matrix(local_space_translation, offset, scale);
+        let (plane_local_matrix, local_plane_matrix) = wasm_parser::outlines::plane_matrices(plane, local_space_translation);
+        let model_to_plane_mat = local_plane_matrix * model_local_matrix * wasm_parser::outlines::DENORM_MATRIX;
+
+        b.iter(|| {
+            let idx = test::black_box(idx.as_slice());
+            let pos = test::black_box(pos.as_slice());
+            let model_to_plane_mat = test::black_box(model_to_plane_mat);
+            let output = test::black_box(output.as_mut_slice());
+            wasm_parser::outlines::intersect_triangles(idx, pos, model_to_plane_mat, output);
+            arena.reset();
         });
     }
 }
