b9bbe15db53a8feb62f06525f0072003a3d6f132
Arturo Castro <arturo@rin.rs>
2023-10-31 08:52:06 +0100

    use nalgebra to do AoSoA simd
    No noticeable performance gains

--- src/outlines.rs
+++ src/outlines.rs
@@ -1,7 +1,8 @@
 
 use crate::{types_2_1::*, parser::{_2_1::{NodeData, ReturnSubMesh}, Indices}};
 use densevec::DenseVec;
-use glam::*;
+use na::*;
+use na::simd::WideF32x4;
 use js_sys::{Uint16Array, Int16Array, Uint32Array, Float32Array};
 use wasm_bindgen::prelude::wasm_bindgen;
 
@@ -51,22 +52,22 @@ impl<'a> OctreeNode<'a> {
         // TODO: we are losing precision here, offset is f64
         let (ox, oy, oz) = (self.data.offset.x as f32, self.data.offset.y as f32, self.data.offset.z as f32);
         let (tx, ty, tz) = (local_space_translation.x, local_space_translation.y, local_space_translation.z);
-        mat4(
-            vec4(scale, 0., 0., 0.),
-            vec4(0., scale, 0., 0.),
-            vec4(0., 0., scale, 0.),
-            vec4(ox - tx, oy - ty, oz - tz, 1.),
+        Mat4::new(
+            scale, 0., 0., 0.,
+            0., scale, 0., 0.,
+            0., 0., scale, 0.,
+            ox - tx, oy - ty, oz - tz, 1.,
         )
     }
 
     pub fn intersects_plane(&self, plane: Vec4) -> bool {
-        let distance = plane.dot(self.center4());
+        let distance = plane.dot(&self.center4());
         if distance.abs() > self.radius() {
             return false
         }
 
         let mut side = 0.;
-        self.corners().into_iter().any(|corner| {
+        self.corners().iter().any(|corner| {
             let distance = plane.dot(corner);
             let dist_sign = distance.signum();
             if side != 0. && dist_sign != side {
@@ -98,11 +99,11 @@ pub struct RenderNode<'a> {
 const DENORM_MATRIX: Mat4 = {
     let s = 1. / 32767.5;
     let o = 0.5 * s;
-    mat4(
-        vec4(s, 0., 0., 0.),
-        vec4(0., s, 0., 0.),
-        vec4(0., 0., s, 0.),
-        vec4(o, o, o, 1.),
+    Mat4::new(
+        s, 0., 0., 0.,
+        0., s, 0., 0.,
+        0., 0., s, 0.,
+        o, o, o, 1.,
     )
 };
 
@@ -237,8 +238,123 @@ pub fn intersect_triangles<I: Into<u32> + Copy>(idx: &[I], pos: &[i16], model_to
 
     debug_assert_eq!(idx.len() % 3, 0);
 
+    let model_to_plane_matx4: Mat4<WideF32x4> = Mat4::from_fn(|i,j| WideF32x4::splat(model_to_plane_mat[(i,j)]));
+
     let mut n = 0;
-    for triangle in idx.chunks(3) {
+    for triangle in idx.chunks_exact(3*4) {
+        let i0 = triangle[0].into() as usize;
+        let i1 = triangle[1].into() as usize;
+        let i2 = triangle[2].into() as usize;
+        let i3 = triangle[3].into() as usize;
+        let i4 = triangle[4].into() as usize;
+        let i5 = triangle[5].into() as usize;
+        let i6 = triangle[6].into() as usize;
+        let i7 = triangle[7].into() as usize;
+        let i8 = triangle[8].into() as usize;
+        let i9 = triangle[9].into() as usize;
+        let i10 = triangle[10].into() as usize;
+        let i11 = triangle[11].into() as usize;
+
+        let p0 = vec4(
+            [pos[i0 * 3 + 0] as f32, pos[i3 * 3 + 0] as f32, pos[i6 * 3 + 0] as f32, pos[i9 * 3 + 0] as f32].into(),
+            [pos[i0 * 3 + 1] as f32, pos[i3 * 3 + 1] as f32, pos[i6 * 3 + 1] as f32, pos[i9 * 3 + 1] as f32].into(),
+            [pos[i0 * 3 + 2] as f32, pos[i3 * 3 + 2] as f32, pos[i6 * 3 + 2] as f32, pos[i9 * 3 + 2] as f32].into(),
+            WideF32x4::splat(1.),
+        );
+        let p1 = vec4(
+            [pos[i1 * 3 + 0] as f32, pos[i4 * 3 + 0] as f32, pos[i7 * 3 + 0] as f32, pos[i10 * 3 + 0] as f32].into(),
+            [pos[i1 * 3 + 1] as f32, pos[i4 * 3 + 1] as f32, pos[i7 * 3 + 1] as f32, pos[i10 * 3 + 1] as f32].into(),
+            [pos[i1 * 3 + 2] as f32, pos[i4 * 3 + 2] as f32, pos[i7 * 3 + 2] as f32, pos[i10 * 3 + 2] as f32].into(),
+            WideF32x4::splat(1.),
+        );
+        let p2 = vec4(
+            [pos[i2 * 3 + 0] as f32, pos[i5 * 3 + 0] as f32, pos[i8 * 3 + 0] as f32, pos[i11 * 3 + 0] as f32].into(),
+            [pos[i2 * 3 + 1] as f32, pos[i5 * 3 + 1] as f32, pos[i8 * 3 + 1] as f32, pos[i11 * 3 + 1] as f32].into(),
+            [pos[i2 * 3 + 2] as f32, pos[i5 * 3 + 2] as f32, pos[i8 * 3 + 2] as f32, pos[i11 * 3 + 2] as f32].into(),
+            WideF32x4::splat(1.),
+        );
+
+        let p0 = model_to_plane_matx4 * p0;
+        let p1 = model_to_plane_matx4 * p1;
+        let p2 = model_to_plane_matx4 * p2;
+
+        // for i in 0..4 {
+        //     let p0 = unsafe{ vec3(p0.x.extract_unchecked(i), p0.y.extract_unchecked(i), p0.z.extract_unchecked(i)) };
+        //     let p1 = unsafe{ vec3(p1.x.extract_unchecked(i), p1.y.extract_unchecked(i), p1.z.extract_unchecked(i)) };
+        //     let p2 = unsafe{ vec3(p2.x.extract_unchecked(i), p2.y.extract_unchecked(i), p2.z.extract_unchecked(i)) };
+
+        //     let gt0 = p0.z > 0.; let gt1 = p1.z > 0.; let gt2 = p2.z > 0.;
+        //     let lt0 = p0.z < 0.; let lt1 = p1.z < 0.; let lt2 = p2.z < 0.;
+        //     if (gt0 || gt1 || gt2) && (lt0 || lt1 || lt2) {
+        //         if intersect_edge(p0, p1, &mut emit) { n+=1 };
+        //         if intersect_edge(p1, p2, &mut emit) { n+=1 };
+        //         if intersect_edge(p2, p0, &mut emit) { n+=1 };
+        //         debug_assert_eq!(n % 2, 0)
+        //     }
+        // }
+
+
+        let zeros = WideF32x4::splat(0.);
+        let gt0 = p0.z.simd_gt(zeros); let gt1 = p1.z.simd_gt(zeros); let gt2 = p2.z.simd_gt(zeros);
+        let lt0 = p0.z.simd_lt(zeros); let lt1 = p1.z.simd_lt(zeros); let lt2 = p2.z.simd_lt(zeros);
+        let cond = ((gt0 | gt1 | gt2) & (lt0 | lt1 | lt2)).bitmask();
+        if cond != 0 {
+            let cond0 = ((p0.z.simd_le(zeros) & p1.z.simd_gt(zeros)) | (p1.z.simd_le(zeros) & p0.z.simd_gt(zeros))).bitmask() & cond;
+            let ab0 = if cond0 != 0 {
+                Some(intersect_edge_x4(p0, p1))
+            }else{
+                None
+            };
+
+            let cond1 = ((p1.z.simd_le(zeros) & p2.z.simd_gt(zeros)) | (p2.z.simd_le(zeros) & p1.z.simd_gt(zeros))).bitmask() & cond;
+            let ab1 = if cond1 != 0 {
+                Some(intersect_edge_x4(p1, p2))
+            }else{
+                None
+            };
+
+            let cond2 = ((p2.z.simd_le(zeros) & p0.z.simd_gt(zeros)) | (p0.z.simd_le(zeros) & p2.z.simd_gt(zeros))).bitmask() & cond;
+            let ab2 = if cond2 != 0 {
+                Some(intersect_edge_x4(p2, p0))
+            }else{
+                None
+            };
+
+            for i in 0..4 {
+                // let p0 = unsafe{ vec3(p0.x.extract_unchecked(i), p0.y.extract_unchecked(i), p0.z.extract_unchecked(i)) };
+                // let p1 = unsafe{ vec3(p1.x.extract_unchecked(i), p1.y.extract_unchecked(i), p1.z.extract_unchecked(i)) };
+                // let p2 = unsafe{ vec3(p2.x.extract_unchecked(i), p2.y.extract_unchecked(i), p2.z.extract_unchecked(i)) };
+                // let gt0 = p0.z > 0.; let gt1 = p1.z > 0.; let gt2 = p2.z > 0.;
+                // let lt0 = p0.z < 0.; let lt1 = p1.z < 0.; let lt2 = p2.z < 0.;
+                // if (gt0 || gt1 || gt2) && (lt0 || lt1 || lt2) {
+                // if (cond >> i) & 0x1 != 0 {
+                    // if (p0.z <= 0. && p1.z > 0.) || (p1.z <= 0. && p0.z > 0.) {
+                    if (cond0 >> i) & 0x1 != 0 {
+                        let (a0, b0) = unsafe{ ab0.unwrap_unchecked() };
+                        unsafe{ emit(a0.extract_unchecked(i), b0.extract_unchecked(i)) }
+                        n += 1;
+                    }
+                    // if (p1.z <= 0. && p2.z > 0.) || (p2.z <= 0. && p1.z > 0.) {
+                    if (cond1 >> i) & 0x1 != 0 {
+                        let (a1, b1) = unsafe{ ab1.unwrap_unchecked() };
+                        unsafe{ emit(a1.extract_unchecked(i), b1.extract_unchecked(i)) }
+                        n += 1;
+                    }
+                    // if (p2.z <= 0. && p0.z > 0.) || (p0.z <= 0. && p2.z > 0.) {
+                    if (cond2 >> i) & 0x1 != 0 {
+                        let (a2, b2) = unsafe{ ab2.unwrap_unchecked() };
+                        unsafe{ emit(a2.extract_unchecked(i), b2.extract_unchecked(i)) }
+                        n += 1;
+                    }
+
+
+                // }
+            }
+        }
+        debug_assert_eq!(n % 2, 0)
+    }
+
+    for triangle in idx[idx.len() - idx.len() % 12 ..].chunks(3) {
         let i0 = triangle[0].into() as usize;
         let i1 = triangle[1].into() as usize;
         let i2 = triangle[2].into() as usize;
@@ -253,9 +369,9 @@ pub fn intersect_triangles<I: Into<u32> + Copy>(idx: &[I], pos: &[i16], model_to
         let gt0 = p0.z > 0.; let gt1 = p1.z > 0.; let gt2 = p2.z > 0.;
         let lt0 = p0.z < 0.; let lt1 = p1.z < 0.; let lt2 = p2.z < 0.;
         if (gt0 || gt1 || gt2) && (lt0 || lt1 || lt2) {
-            if intersect_edge(p0, p1, &mut emit) { n+=1 };
-            if intersect_edge(p1, p2, &mut emit) { n+=1 };
-            if intersect_edge(p2, p0, &mut emit) { n+=1 };
+            if intersect_edge(p0.xyz(), p1.xyz(), &mut emit) { n+=1 };
+            if intersect_edge(p1.xyz(), p2.xyz(), &mut emit) { n+=1 };
+            if intersect_edge(p2.xyz(), p0.xyz(), &mut emit) { n+=1 };
             debug_assert_eq!(n % 2, 0)
         }
     }
@@ -268,7 +384,7 @@ pub struct WasmMat4(Mat4);
 
 #[wasm_bindgen]
 pub fn allocate_mat4(mat: &[f32]) -> WasmMat4 {
-    WasmMat4(Mat4::from_cols_slice(mat))
+    WasmMat4(Mat4::from_column_slice(mat))
 }
 
 #[wasm_bindgen]
@@ -318,7 +434,7 @@ pub fn intersect_triangles_u32(
 }
 
 #[inline(always)]
-fn intersect_edge(p0: Vec4, p1: Vec4, mut emit: impl FnMut(f32, f32)) -> bool {
+fn intersect_edge(p0: Vec3, p1: Vec3, mut emit: impl FnMut(f32, f32)) -> bool {
     if (p0.z <= 0. && p1.z > 0.) || (p1.z <= 0. && p0.z > 0.) {
         let t = -p0.z / (p1.z - p0.z);
         emit(lerp(p0.x, p1.x, t), lerp(p0.y, p1.y, t));
@@ -330,5 +446,18 @@ fn intersect_edge(p0: Vec4, p1: Vec4, mut emit: impl FnMut(f32, f32)) -> bool {
 
 #[inline(always)]
 fn lerp(a: f32, b: f32, t: f32) -> f32 {
+    a + (b - a) * t
+}
+
+#[inline(always)]
+fn intersect_edge_x4(p0: Vec4<WideF32x4>, p1: Vec4<WideF32x4>) -> (WideF32x4, WideF32x4) {
+    let t = -p0.z / (p1.z - p0.z);
+    let a = lerp_x4(p0.x, p1.x, t);
+    let b = lerp_x4(p0.y, p1.y, t);
+    (a, b)
+}
+
+#[inline(always)]
+fn lerp_x4(a: WideF32x4, b: WideF32x4, t: WideF32x4) -> WideF32x4 {
     a + (b - a) * t
 }
